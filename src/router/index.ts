bc9928bf6aa02de0808c22f132cee8cd0ed56e26
Lê Ý <duongy96@gmail.com>
2024-04-17 00:03:59 +0700

    Config

--- src/router/index.ts
+++ src/router/index.ts
@@ -175,7 +175,7 @@ export const contactRoute = getRoute('contact');
 export const aboutRoute = getRoute('aboutUs');
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.VITE_BASE_URL),
   routes,
 });
 
